b0e7f13a2641c86c6a923059b190ce12878c783b
KCroce <kaiodellacroce@gmail.com>
2024-11-09 21:30:35 -0300

    Normalizando Android

---
+++ app/src/main/java/com/example/androidfinal/login/Login.java
@@ -0,0 +1,18 @@
+package com.example.androidfinal.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.androidfinal.R;
+
+
+public class Login extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+    }
+}
