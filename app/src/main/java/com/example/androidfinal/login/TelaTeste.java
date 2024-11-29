f14653d8929db5be90733e4f7b9b57c16ec7673f
KCroce <kaiodellacroce@gmail.com>
2024-11-29 10:13:43 -0300

    Feature:Login + tela generica

---
+++ app/src/main/java/com/example/androidfinal/login/TelaTeste.java
@@ -0,0 +1,13 @@
+package com.example.androidfinal.login;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidfinal.R;
+
+public class TelaTeste extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        setContentView(R.layout.pos_login);
+        super.onCreate(savedInstanceState);
+    }
+}
